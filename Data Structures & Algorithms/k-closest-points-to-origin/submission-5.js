0de99741069d1d0eb3bdb2f1c4fed1184f7989df
Max Dragaytsev <40897499+dmax1447@users.noreply.github.com>
2026-04-02 14:48:55 +0300

    Add: k-closest-points-to-origin - submission-5

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-5.js
@@ -0,0 +1,103 @@
+class Solution {
+    /**
+     * @param {number[][]} points
+     * @param {number} k
+     * @return {number[][]}
+     */
+    kClosest(points, k) {
+        function getDistanseFromOrigin(point) {
+            const [x, y] = point
+            return Math.sqrt(x**2 + y**2)
+        }
+        function compareByDistance2(a, b) {
+            const distA = getDistanseFromOrigin(a)
+            const distB = getDistanseFromOrigin(b)
+            return distA - distB
+        }
+        function compareByCoords(a, b) {
+            const distA = a[0]**2 +a[1]**2
+            const distB = b[0]**2 + b[1]**2
+            return distA - distB
+        }
+        const pointsHeap = new MaxHeap(points, compareByCoords)
+        while(pointsHeap.getLength() > k) {
+            pointsHeap.pop()
+        }
+        return pointsHeap.getData()
+    }
+}
+
+class MaxHeap {
+    constructor(arr, compareFn) {
+        this.compareFn = typeof compareFn === 'function' ? compareFn : (a, b) => a - b
+        this.heapify(arr)
+    }
+
+    getData() {
+        return this.heap.slice(1)
+    }
+
+    isLarger(idxA, idxB) {
+        if (!this.heap[idxA]) return false
+        if (!this.heap[idxB]) return true
+        return this.compareFn(this.heap[idxA], this.heap[idxB]) > 0
+    }
+
+    swap(idxA, idxB) {
+        const tmp = this.heap[idxA]
+        this.heap[idxA] = this.heap[idxB]
+        this.heap[idxB] = tmp
+    }
+
+    getLength() {
+        return this.heap.length - 1
+    }
+
+    peek() {
+        return this.heap[1] ?? 0
+    }
+
+    heapify(arr) {
+        this.heap = arr.slice()
+        this.heap.push(this.heap[0])
+        this.heap[0] = 0
+        let i = Math.floor((this.heap.length - 1) / 2)
+        while(i > 0) {    
+            this.percolateDown(i)
+            i--
+        }
+    }
+
+    percolateUp(idx) {
+        while(idx > 1 && this.isLarger(idx, Math.floor(idx / 2))) {
+            this.swap(Math.floor(idx / 2), idx)
+            idx = Math.floor(idx / 2)
+        }
+    }
+
+    percolateDown(i) {
+        while((i * 2) < this.heap.length) {
+            const maxChildIdx = this.isLarger(i * 2, i * 2 + 1) ? i * 2 : i * 2 + 1
+            if (this.isLarger(maxChildIdx, i)) {
+                this.swap(i, maxChildIdx)
+                i = maxChildIdx
+            } else {
+                break
+            }
+        }
+    }
+
+    pop() {
+        if (this.getLength() === 1) return this.heap.pop()
+        const max = this.heap[1]
+        const el = this.heap.pop()
+        this.heap[1] = el
+        this.percolateDown(1)
+        return max
+    }
+
+    push(val) {
+        this.heap.push(val)
+        this.percolateUp(this.heap.length - 1)
+    }
+}
