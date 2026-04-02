2b33d9b21baf0855636792412b926be1233fce62
Max Dragaytsev <40897499+dmax1447@users.noreply.github.com>
2026-04-02 16:50:00 +0300

    Add: k-closest-points-to-origin - submission-6

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-6.js
@@ -0,0 +1,103 @@
+// Solution 3
+
+class Solution {
+    /**
+     * @param {number[][]} points
+     * @param {number} k
+     * @return {number[][]}
+     */
+    kClosest(points, k) {
+        function compareByCoords(a, b) {
+            const distA = a[0]**2 +a[1]**2
+            const distB = b[0]**2 + b[1]**2
+            return distA - distB
+        }
+        const pointsHeap = new MyHeap(points, compareByCoords)
+        while(pointsHeap.getLength() > k) {
+            pointsHeap.pop()
+        }
+        return pointsHeap.getData()
+    }
+}
+
+class MyHeap {
+  constructor(arr, priorityFn) {
+    //
+    /*
+    * функция определения приоритета, более самый приоритетный идет наверх кучи.
+    * Как сортировка, если a приоритетнее b то ожидается положительное значение, если нет - отрицательное
+    * MaxHeap: (a, b) => a - b
+    * MinHeap: (a, b) => b - a
+    * */
+    this.priorityFn = priorityFn
+    this.heapify(arr)
+  }
+
+  getData() {
+    return this.heap.slice(1)
+  }
+
+  isHigherPriority(idxA, idxB) {
+    if (!this.heap[idxA]) return false
+    if (!this.heap[idxB]) return true
+    return this.priorityFn(this.heap[idxA], this.heap[idxB]) > 0
+  }
+
+  swap(idxA, idxB) {
+    const tmp = this.heap[idxA]
+    this.heap[idxA] = this.heap[idxB]
+    this.heap[idxB] = tmp
+  }
+
+  getLength() {
+    return this.heap.length - 1
+  }
+
+  peek() {
+    return this.heap[1] ?? 0
+  }
+
+  heapify(arr) {
+    this.heap = arr.slice()
+    this.heap.push(this.heap[0])
+    this.heap[0] = 0
+    let i = Math.floor((this.heap.length - 1) / 2)
+    while(i > 0) {
+      this.percolateDown(i)
+      i--
+    }
+  }
+
+  percolateUp(idx) {
+    while(idx > 1 && this.isHigherPriority(idx, Math.floor(idx / 2))) {
+      this.swap(Math.floor(idx / 2), idx)
+      idx = Math.floor(idx / 2)
+    }
+  }
+
+  percolateDown(i) {
+    while((i * 2) < this.heap.length) {
+      const maxChildIdx = this.isHigherPriority(i * 2, i * 2 + 1) ? i * 2 : i * 2 + 1
+      if (this.isHigherPriority(maxChildIdx, i)) {
+        this.swap(i, maxChildIdx)
+        i = maxChildIdx
+      } else {
+        break
+      }
+    }
+  }
+
+  pop() {
+    if (this.getLength() === 1) return this.heap.pop()
+    const max = this.heap[1]
+    const el = this.heap.pop()
+    this.heap[1] = el
+    this.percolateDown(1)
+    return max
+  }
+
+  push(val) {
+    this.heap.push(val)
+    this.percolateUp(this.heap.length - 1)
+  }
+}
